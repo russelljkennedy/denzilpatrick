f18a0f113194d0ee7e858679c0dab3ed8789bb5b
Russell Kennedy <russell.j.kennedy@googlemail.com>
2021-03-14 00:12:06 +0000

    Store JS

--- js/store.js
+++ js/store.js
@@ -1,10 +1,13 @@
 let selectedSize = Array.from(document.querySelectorAll(".size"));
+const addBtn = document.getElementById("add-btn");
 
 selectedSize.forEach(function(e) {
   e.addEventListener("click", function() {
     selectedSize.forEach(function(e) {
       e.classList.remove("selected");
+      addBtn.setAttribute("data-item-custom1-value", "");
     });
     e.classList.add("selected");
+    addBtn.setAttribute("data-item-custom1-value", e.innerHTML);
   });
 });
